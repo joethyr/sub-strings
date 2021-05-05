fa683dba86276d3fca515e40fcf1806c25c0714c
Joseph Thayer <joethayr@gmail.com>
2021-05-04 21:42:34 -0600

    Fixed method. Now first argument takes multiple words.

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,11 +1,14 @@
 dictionary = ['below', 'down', 'below', 'go', 'going', 'horn', 'how', 'howdy', 'it', 'i', 'low', 'own', 'part', 'partner', 'sit']
 
-def substrings(word, list)
+def substrings(word, dictionary)
   hash_list = {}
-  if list.include?(word)
-    hash_list[word] = list.count(word)
+  array_word = word.gsub(/[[:punct:]]/, '').split(' ')
+  array_word.each do |e|
+    if dictionary.include?(e)
+      hash_list[e] = dictionary.count(e)
+    end
   end
   puts hash_list
 end
 
-substrings('below', dictionary)
+substrings('below, down go!', dictionary)
