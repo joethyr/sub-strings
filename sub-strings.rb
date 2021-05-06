c30cbfc5531a42f3558d761fd1463a1a782f7b70
Joseph Thayer <joethayr@gmail.com>
2021-05-05 19:14:00 -0600

    modified method to be case insensitive.

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,14 +1,15 @@
-dictionary = ['below', 'down', 'below', 'go', 'going', 'horn', 'how', 'howdy', 'it', 'i', 'low', 'own', 'part', 'partner', 'sit']
+dictionary = ['Below', 'Down', 'below', 'go', 'going', 'horn', 'how', 'howdy', 'it', 'i', 'low', 'own', 'part', 'partner', 'sit']
 
 def substrings(word, dictionary)
   hash_list = {}
-  array_word = word.gsub(/[[:punct:]]/, '').split(' ')
+  array_word = word.downcase.gsub(/[[:punct:]]/, '').split(' ')
+  downcase_dictionary = dictionary.map(&:downcase)
   array_word.each do |e|
-    if dictionary.include?(e)
-      hash_list[e] = dictionary.count(e)
+    if downcase_dictionary.include?(e)
+      hash_list[e] = downcase_dictionary.count(e)
     end
   end
   puts hash_list
 end
 
-substrings('below, down go!', dictionary)
+substrings('Below, down go!', dictionary)
